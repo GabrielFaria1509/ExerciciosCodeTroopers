34f49f0a1988ed11e41f72c9a62bd568bc53b6b7
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-05-19 16:46:56 -0300

    Update Lucro.cpp

--- Lucro.cpp
+++ Lucro.cpp
@@ -1,37 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
-    int n;
-    int contador;
-    int Lucrototal = 0;
-    int somareceita;
-    int lucroestadia;
-    int receitadia;
-    int custoestadia;
-    int custodia;
-
-    while(cin >> n){
-        cin >> custodia;
-
-        custoestadia = custodia * n;
-        cin >> receitadia;
-        somareceita+= receitadia;
-        contador++;
-        if(contador == n){
-            lucroestadia = somareceita - custoestadia;
-            Lucrototal += lucroestadia;
+int main() {
+    int n; // número de dias
+    int custoPorDia;
+    int receita[50];
+    
+    while (cin >> n) {
+        cin >> custoPorDia;
+        
+        for (int i = 0; i < n; i++) {
+            cin >> receita[i];
         }
-
-        receitadia = 0;
-        custoestadia = 0;
-        somareceita = 0;
-        contador = 0;
-
+        
+        int max_lucro = 0;
+        int lucro_atual = 0;
+        
+        for (int i = 0; i < n; i++) {
+            // Calcula o lucro líquido para o dia i
+            int lucro_dia = receita[i] - custoPorDia;
+            
+            // Atualiza o lucro atual (soma contígua)
+            lucro_atual += lucro_dia;
+            
+            // Se o lucro atual ficar negativo, reinicia (melhor começar nova sequência)
+            if (lucro_atual < 0) {
+                lucro_atual = 0;
+            }
+            
+            // Atualiza o máximo encontrado
+            if (lucro_atual > max_lucro) {
+                max_lucro = lucro_atual;
+            }
+        }
+        
+        cout << max_lucro << endl;
     }
-
-    cout << Lucrototal << endl;
-
+    
     return 0;
 }
