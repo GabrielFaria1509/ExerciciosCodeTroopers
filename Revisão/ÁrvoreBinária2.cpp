50338dc6fb4b8ef292d4666f53460a71e9fe4019
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-04-01 14:18:58 -0300

    Create ÁrvoreBinária2.cpp

---
+++ Revisão/ÁrvoreBinária2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool predicate_function(int mid) {
+
+ // if it satisfies the condition then return true
+ // else return false
+}
+
+int manin() {
+
+ int low, high, mid;
+
+ // Low is the minimum value
+ low = Minimum value;
+ // High is the maximum value
+ high = Maximum value;
+
+ while (high > low + 1) {
+  mid = (low + high) / 2;
+
+  if (predicate_function(mid))
+   True Part = mid;
+  else
+   False Part = mid;
+ }
+
+ // Ans might be low/high based on the question
+ cout << Ans << endl;
+}
