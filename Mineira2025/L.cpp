c4f109a95ce41b04608b5337038d4d51c1872684
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-05-31 17:46:19 -0300

    Create L.cpp

---
+++ Mineira2025/L.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    //problema interativo maratona mineira de programação 2025(problema L) 
+    long long int a;
+    long int b;
+    long int c;
+
+
+    cin >> a >> b >> c;
+
+    if(a*b == c) {
+        cout << "S" << endl;
+    } else {
+        cout << "N" << endl;
+    }
+
+
+
+    
+    
+
+    return 0;
+}
